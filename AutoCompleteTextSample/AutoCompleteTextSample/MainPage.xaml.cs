6da218332745aa3cb90a09d2df3fead882ccc5b2
cemozguraA <55392106+cemozguraA@users.noreply.github.com>
2020-02-25 11:47:55 +0300

    Update MainPage.xaml.cs

--- AutoCompleteTextSample/AutoCompleteTextSample/MainPage.xaml.cs
+++ AutoCompleteTextSample/AutoCompleteTextSample/MainPage.xaml.cs
@@ -74,7 +74,7 @@ namespace sample
         }
         public ObservableCollection<test> list = new ObservableCollection<test>();
         public ObservableCollection<test> list2 = new ObservableCollection<test>();
-        public ObservableCollection<test> list3 = new ObservableCollection<test>();
+ 
 
         public MainPage()
         {
@@ -86,17 +86,12 @@ namespace sample
             list.Add(new test { Name = "Xamarin.Android" ,Image= "https://cdnp2.stackassets.com/85735c127703a5ac1c8ccfdd37d4a6ab745d351f/store/opt/596/298/e05b1bcee2d95c5df7fbdcde0ac788bf47d51c7ca2517ab55ae3b2e8f354/d51f324692f2e52f5987c4293792ead54457b98f_main_hero_image.jpg" });
             list.Add(new test { Name = "Xamarin.IOS" ,Image= "https://sezeromer.com/wp-content/uploads/2018/07/xamarin-ios.png" });
             list.Add(new test { Name = "Xamarin.Forms", Image = "https://www.html.it/1XlSAnUzNqYnvNphCBCva__vPd0=/1280x854/smart/app/uploads/2017/09/xamarin.png" });
-            list.Add(new test { Name = "Rise", Image = "https://media-exp1.licdn.com/dms/image/C4D0BAQEUM8B4HQi2-A/company-logo_200_200/0?e=1586995200&v=beta&t=TdCF6MqbiJemx2b_UjgnvYy08m7Cxcth_46IABaLE5s" });
-            list.Add(new test { Name = "Xamarin", Image = "https://www.html.it/1XlSAnUzNqYnvNphCBCva__vPd0=/1280x854/smart/app/uploads/2017/09/xamarin.png" });
-            list.Add(new test { Name = "Xamarin.Android", Image = "https://cdnp2.stackassets.com/85735c127703a5ac1c8ccfdd37d4a6ab745d351f/store/opt/596/298/e05b1bcee2d95c5df7fbdcde0ac788bf47d51c7ca2517ab55ae3b2e8f354/d51f324692f2e52f5987c4293792ead54457b98f_main_hero_image.jpg" });
-            list.Add(new test { Name = "Xamarin.IOS", Image = "https://sezeromer.com/wp-content/uploads/2018/07/xamarin-ios.png" });
             list.Add(new test { Name = "Microsft" ,Image= "https://www.dunya.com/uploads/content/microsoft-logopng_daz01.png?v=1577734478" });
             list.Add(new test { Name = "Cem Özgür",Image= "https://i.imgyukle.com/2020/01/13/VGNBtA.jpg" });
             list.Add(new test { Name = "ipman",Image= "https://sm.mashable.com/t/mashable_sea/photo/default/ip-man-header_ceft.960.jpg" });
             list.Add(new test { Name = "ipek",Image= "https://www.marketimgeldi.com/Uploads/UrunResimleri/konya-etli-ekmek---porsiyon-977b.png" });
             list2.Add(new test { Name = "ipek",Image= "https://www.marketimgeldi.com/Uploads/UrunResimleri/konya-etli-ekmek---porsiyon-977b.png" });
-            list3.Add(new test { Name = "ipek",Image= "https://www.marketimgeldi.com/Uploads/UrunResimleri/konya-etli-ekmek---porsiyon-977b.png" });
-            list3.Add(new test { Name = "ipek",Image= "https://www.marketimgeldi.com/Uploads/UrunResimleri/konya-etli-ekmek---porsiyon-977b.png" });
+            
 
             auto.ItemsSource = list;
             auto.SelectedItemsSource = list2;
@@ -145,14 +140,13 @@ namespace sample
         }
         private void Button1_Clicked(object sender, EventArgs e)
         {
-            list3.Add(new test { Name = "ipman", Image = "https://sm.mashable.com/t/mashable_sea/photo/default/ip-man-header_ceft.960.jpg" });
-            list3.Add(new test { Name = "ipek", Image = "https://www.marketimgeldi.com/Uploads/UrunResimleri/konya-etli-ekmek---porsiyon-977b.png" });
+            list2.RemoveAt(0);
         }
         private void Button2_Clicked(object sender, EventArgs e)
         {
-            auto.SelectedItemsSource = list3;
+            
             //list2.RemoveAt(2);
-            //list2.Clear();
+            list2.Clear();
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
